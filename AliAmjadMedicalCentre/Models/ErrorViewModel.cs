a2118dae85ad5a5f04bcb49951e04d74a22a9870
Ali0512 <106133675+Ali0512@users.noreply.github.com>
2022-12-30 18:48:30 +0500

    Add project files.

---
+++ AliAmjadMedicalCentre/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AliAmjadMedicalCentre.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
